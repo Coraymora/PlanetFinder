60308ebf247877178c51f9982c31ecb41354450c
Coraymora <jacobsanders237@gmail.com>
2022-09-11 10:26:24 -0400

    Final Commit for Shellhacks 2022

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route("/hello")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    return render_template('index.html')
 
+@app.route('/result/', methods=['POST'])
+def result():
+    location = request.form.get("location")
+    planet = request.form.get("planet")
+    
+    return render_template('result.html')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
